fddb5b03688f48a23b68819a71fa31b44b001e38
Will3116 <0310wiaz501@gmail.com>
2026-04-01 18:35:19 +0200

    Trigger Zone
    Har skappat en placeholder trigger zone med trigger zone script.
    Ändrade min Animation Script för att passa trigger zone.
    Ändrade Player i inspect (la till Player Tag + Rigid body) för att passa trigger zone.

---
+++ Assets/Scripts/Animation/PlaceholderAnimationScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class PlaceholderAnimationScript : MonoBehaviour    //script för placeholder animation. När jag trycker "P" spelas animationen
+{
+    private Animator animator;
+
+    public TriggerZonePlaceholder zone;     //asign en "zone" till animation i inspect
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) && zone.PlayerInsideZone)       //animation kan bara hända om PlayerInsideZone bool = true
+        {
+            animator.SetTrigger("isFalling");
+        }
+    }
+}
