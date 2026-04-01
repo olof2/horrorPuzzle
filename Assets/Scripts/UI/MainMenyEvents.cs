7e61ff56fc3cfd52be07e8d549eae096c80ec3ec
Hugge252 <hugoand04@gmail.com>
2026-04-01 18:17:50 +0200

    Sanity meter system (simple)

--- Assets/Scripts/UI/MainMenyEvents.cs
+++ Assets/Scripts/UI/MainMenyEvents.cs
@@ -5,11 +5,15 @@ using UnityEngine.UIElements;
 public class MainMenyEvents : MonoBehaviour
 {
     private UIDocument document;
+    private UIDocument sanityDocument;
     private Button button;
     private PlayerMovement playerMovement;
     private PlayerCameraLook playerCameraLook;
 
-    private VisualTreeAsset startMenu;
+    private SanityMeter sanityMeter;
+    private SanityMeterUI sanityMeterUI;
+
+    private VisualTreeAsset SanityMeterUI;
     private VisualTreeAsset pausMenu;
 
     
@@ -18,11 +22,21 @@ public class MainMenyEvents : MonoBehaviour
 
     private void Awake()
     {
+        //Hittar dokumentet och knapparna i main meny, disablar spelkontrollerna och sanity meter.
         document = GetComponent<UIDocument>();
+        sanityDocument = GetComponent<UIDocument>();
+        
+        //Stänker av spelkontrollerna och sanity meter så att de inte kan användas i main meny.
         playerMovement = FindFirstObjectByType<PlayerMovement>();
         playerCameraLook = FindFirstObjectByType<PlayerCameraLook>();
         playerMovement.enabled = false;
         playerCameraLook.enabled = false;
+        
+
+        sanityMeter = FindFirstObjectByType<SanityMeter>();
+        sanityMeter.enabled = false;
+
+        
 
 
         button = document.rootVisualElement.Q("StartGameButton") as Button;
@@ -34,13 +48,13 @@ public class MainMenyEvents : MonoBehaviour
             menuButtons[i].RegisterCallback<ClickEvent>(OnAllButtonsClick);
         }
 
-        
     }
 
 
     private void OnDisable()
     {
         button.UnregisterCallback < ClickEvent>(OnPlayGameClick);
+        
         for (int i = 0; i < menuButtons.Count; i++)
         {
             menuButtons[i].UnregisterCallback<ClickEvent>(OnAllButtonsClick);
@@ -51,12 +65,20 @@ public class MainMenyEvents : MonoBehaviour
     {
         Debug.Log("You pressed the Start Button");
 
+        //Enables spelkontrollerna och sanity meter efter man har startat spelet, disablar main meny dokumentet så att det inte syns längre.
         playerMovement = FindFirstObjectByType<PlayerMovement>();
         playerCameraLook = FindFirstObjectByType<PlayerCameraLook>();
         playerMovement.enabled = true;
         playerCameraLook.enabled = true;
 
+        sanityMeter = FindFirstObjectByType<SanityMeter>();
+        //Enables sanity meter så att den kan användas i spelet.
+        sanityMeter.enabled = true;
+        //Enable sanity metern UI
+        
+        //Disablar main meny dokumentet så att det inte syns längre.
         document.enabled = false;
+        sanityDocument.enabled = true;
 
 
 
