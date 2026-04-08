85b0635a01ec41fb27e315b3ecd89ad4126f8ec2
Spinny <Olle@24.se>
2026-04-08 19:26:14 +0200

    trewwrtwrtq

--- Assets/Scripts/UI/MainMenyEvents.cs
+++ Assets/Scripts/UI/MainMenyEvents.cs
@@ -11,7 +11,7 @@ public class MainMenyEvents : MonoBehaviour
     private PlayerMovement playerMovement;
     private PlayerCameraLook playerCameraLook;
     private PausedMenUScript pausedMenUScript;
-    //private GameOverScript gameOverScript;
+    private GameOverScript gameOverScript;
 
     private SanityMeter sanityMeter;
    
@@ -42,9 +42,9 @@ public class MainMenyEvents : MonoBehaviour
         pausedMenUScript = FindAnyObjectByType<PausedMenUScript>();
         if (pausedMenUScript != null)
             pausedMenUScript.enabled = false;
-        //gameOverScript = FindAnyObjectByType<GameOverScript>();
-        //if (gameOverScript != null)
-        //    gameOverScript.enabled = false;
+        gameOverScript = FindAnyObjectByType<GameOverScript>();
+        if (gameOverScript != null)
+            gameOverScript.enabled = false;
 
 
 
@@ -81,7 +81,7 @@ public class MainMenyEvents : MonoBehaviour
         //Enables spelkontrollerna och sanity meter efter man har startat spelet, disablar main meny dokumentet så att det inte syns längre.
         playerMovement = FindFirstObjectByType<PlayerMovement>();
         playerCameraLook = FindFirstObjectByType<PlayerCameraLook>();
-        //gameOverScript.enabled = true;
+        gameOverScript.enabled = true;
         playerMovement.enabled = true;
         playerCameraLook.enabled = true;
         pausedMenUScript.enabled = true;
