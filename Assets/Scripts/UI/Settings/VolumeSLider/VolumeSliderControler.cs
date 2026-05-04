cf40fdcadb09e517e35de2b003a15a89bc87ddaf
Hugge252 <hugoand04@gmail.com>
2026-05-04 18:40:48 +0200

    Volume slider fix + broken SettingMeny från main meny(work in progress)

--- Assets/Scripts/UI/Settings/VolumeSLider/VolumeSliderControler.cs
+++ Assets/Scripts/UI/Settings/VolumeSLider/VolumeSliderControler.cs
@@ -5,14 +5,22 @@ public class VolumeSliderControler : MonoBehaviour
 {
     [SerializeField]
     private MusicSystem music;
+    //public UIDocument musicSliderDoc;
+    //public UIDocument sfxSliderDoc;
+    //public VisualElement sfxSlider;
+    //public VisualElement musicSlider;
+
 
     private void OnEnable()
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
         root.dataSource = music;
 
-        var slider = root.Q<Slider>("VolumeSlider");
+        var slider = root.Q<Slider>("VolumSlider");
         slider.value = music.Volume;
         slider.RegisterValueChangedCallback(evt => music.Volume = evt.newValue);
+
+        //var sxfRoot = GetComponent<UIDocument>().rootVisualElement;
+        //var sfxSlider = sxfRoot.Q<Slider>("SfxSlider");
     }
 }
